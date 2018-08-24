70e0f050ac838ae50cccf17db92853eb0641f435
lakshyajit165 <lakshyajit165@gmail.com>
2018-08-24 20:31:53 +0530

    More practice

---
+++ balanced_string.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int top = -1;
+void check(string s, int n, string st){
+	
+	for(int i = 0; i<n; i++){
+		if(s[i] == '('){
+			top = top+1;
+			st[top] = '(';
+		}
+		if(s[i] == ')'){
+			if(top == -1){
+				top -= 1;
+				break;
+			}else{	
+				top -= 1;
+			}
+		}
+	}
+	if(top == -1){
+		cout<<"String is Balanced!";		
+	}else{
+		cout<<"String is not Balanced";
+	}
+}
+int main(){
+	
+	string s,st;
+	cin>>s;
+	check(s,s.length(),st);
+	return 0;
+}
---
+++ test_7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int p;
+    cin>>p;
+    while(p!=0){
+        cout<<p<<endl;
+        p--;
+    }
+    return 0;
+}
